aced15026aad0543942ebae49c4bee80adb6f1e2
Miroslav Levdikov <torenux@gmail.com>
2026-03-05 23:06:24 +0200

    Add scroll functionality to ProductDetailsScreen content

--- app/src/main/java/app/skeleton/product/ui/composable/screen/productdetails/ProductDetailsScreen.kt
+++ app/src/main/java/app/skeleton/product/ui/composable/screen/productdetails/ProductDetailsScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -83,6 +85,8 @@ private fun ProductDetailsPopulated(
     modifier: Modifier = Modifier,
     onAddToCart: () -> Unit
 ) {
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
@@ -90,6 +94,7 @@ private fun ProductDetailsPopulated(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
+                .verticalScroll(scrollState)
         ) {
             Image(
                 painter = painterResource(id = product.imageRes),
