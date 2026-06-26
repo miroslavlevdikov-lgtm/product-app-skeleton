4707299f4c93760d0167847a61344284afaedd9b
miroslavlevdikov-lgtm <miroslav.levdikov@asoteam.top>
2026-06-26 18:42:34 +0300

    Первичная расстановка структурных маркеров и других артефактов

--- app/src/main/java/app/skeleton/product/SkeletonApplication.kt
+++ app/src/main/java/app/skeleton/product/SkeletonApplication.kt
@@ -1,23 +1,44 @@
 package app.skeleton.product
 
 import android.app.Application
+//[ANY][import_PrepRepository]
 import app.skeleton.product.di.dataModule
 import app.skeleton.product.di.dispatcherModule
 import app.skeleton.product.di.viewModule
+//[COMMON][import_DiModule]
+//[REFERRER][import_InstallReferrerManager]
+//[APPSFLYER][imports_AppsFlyer]
+//[FIREBASE][import_FirebaseMessaging]
+//[FIREBASE][imports_coroutines]
+//[ANY][import_getKoin]
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 
 class SkeletonApplication : Application() {
+    //[FIREBASE][appScope]
+
     override fun onCreate() {
         super.onCreate()
 
-        val appModules = dataModule + viewModule + dispatcherModule
+        val appModules = dataModule + viewModule + dispatcherModule /*[COMMON][diModule]*/
 
         startKoin {
             androidLogger()
             androidContext(this@SkeletonApplication)
             modules(appModules)
         }
+
+        //[ANY][repository]
+
+        //[APPSFLYER][devKey]
+
+        //[APPSFLYER][appsFlyerSettings]
+
+        //[REFERRER][referrerManagerSettings]
+
+        //[APPSFLYER][appsFlyerId]
+
+        //[FIREBASE][FirebaseMessaging]
     }
 }
