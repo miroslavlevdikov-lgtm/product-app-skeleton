6b071caef719b0a94e9c00384d40c5e208350004
miroslavlevdikov-lgtm <miroslav.levdikov@asoteam.top>
2026-06-27 12:39:52 +0300

    Добавлены маркеры импортов в MainActivity

--- app/src/main/java/app/skeleton/product/MainActivity.kt
+++ app/src/main/java/app/skeleton/product/MainActivity.kt
@@ -1,11 +1,15 @@
 package app.skeleton.product
 
+//[FIREBASE|APPSFLYER][import_Intent]
+//[FIREBASE][import_URI]
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+//[FIREBASE][imports_workmanager_settings]
 import app.skeleton.product.ui.composable.approot.AppRoot
 import app.skeleton.product.ui.theme.ProductAppSkeletonTheme
+//[FIREBASE][import_VisitRequestWorker]
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
